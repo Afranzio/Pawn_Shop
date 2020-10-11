3014a7c1be749e4f4a0e63587ae45ff7919fef32
Afranzio <61235427+Afranzio@users.noreply.github.com>
2020-10-11 13:24:08 +0530

    Add files via upload

--- source.py
+++ source.py
@@ -3,42 +3,34 @@ import eel
 ##Select The Folder of FrontEnd File
 eel.init("web")
 
+
 @eel.expose
-
-mycol=""
-
 def connectDB():
     
     import pymongo
-    import json
-    import pandas
 
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     mydb = myclient["Shop"]
+    global mycol
     mycol = mydb["Pawn"]
 
-    with open(r'E:\Freelance\Manoj\Data\data.json') as f: #Enter your data.json file path
+def addData():
+    import json
+    with open(r'./Data/data.json') as f: #Enter your data.json file path
         data = json.load(f)
     len(data["main"])
-
     for each in data["main"]:
         mycol.insert_one(each)
 
-def fetchUser(Name,Number):
-    
+
+@eel.expose
+def fetchUser(name, number):
 #     Name = str(input("Enter name to filter : ")).title()
-
-    if Name == "":
-
-        Number = str(input("Enter number to filter : "))
-        c = mycol.find({"Number": Number})
-        for d in c:
-            return(d["Name"])
-
-    a = mycol.find({"Name": Name.title()})
-
-    for b in a:
-        return(b)
+    print("working", name.title(), number)
+    c = mycol.find({"Name":name.title(), "Number": number})
+    for d in c:
+        print(d)
+        return(d)
     
 def fetchImg(imgID):
     
@@ -50,20 +42,9 @@ def fetchImg(imgID):
 
     # This method will show image in any image viewer 
     im.show()
-    
-def newUser(Number, Amount,Name,Relation,Location,Place,Item,Pledged,Weight):
-    
-#     Number = str(input("Number : "))
-#     Amount = int(input("Amount : "))
-#     Name = str(input("Name : "))
-#     Relation = str(input("S/O W/O D/O : "))
-#     Location = str(input("Location : "))
-#     Place = str(input("Place : "))
-#     Item_type = str(input("Item type : "))
-#     Item = str(input("Item : "))
-#     Pledged = str(input("Pledged Date : "))
-#     Weight = int(input("Weight : "))
 
+@eel.expose   
+def newUser(Number, Amount,Name,Relation,Location,Place,Item,itemType, mobile, Pledged,Weight):
     mydict={
         "Number" :Number ,
         "Amount" :Amount,
@@ -72,6 +53,8 @@ def newUser(Number, Amount,Name,Relation,Location,Place,Item,Pledged,Weight):
         "Location" : Location ,
         "Place" :Place ,
         "Item" : Item,
+        "Item Type" : itemType,
+        "Mobile" : mobile,
         "Pledged Date" :Pledged,
         "Weight" : Weight
     }
@@ -110,8 +93,11 @@ def interestCal(Name):
 
         print("₹",i['Amount'], "--" ,"₹",int(check*interest), "--" , i['Item'], "--" , i['Weight'] )
 
-def caputre():
+
+@eel.expose
+def capture(Name):
     import cv2 
+
     key = cv2. waitKey(1)
     webcam = cv2.VideoCapture(0)
     while True:
@@ -122,23 +108,23 @@ def caputre():
             cv2.imshow("Capturing", frame)
             key = cv2.waitKey(1)
             if key == ord('s'): 
-                cv2.imwrite(filename='saved_img.jpg', img=frame)
+                cv2.imwrite(filename=Name+'.jpg', img=frame)
                 webcam.release()
-                img_new = cv2.imread('saved_img.jpg', cv2.IMREAD_GRAYSCALE)
+                img_new = cv2.imread('person.jpg', cv2.IMREAD_GRAYSCALE)
                 img_new = cv2.imshow("Captured Image", img_new)
                 cv2.waitKey(1650)
                 cv2.destroyAllWindows()
-                print("Processing image...")
-                img_ = cv2.imread('saved_img.jpg', cv2.IMREAD_ANYCOLOR)
-                print("Converting RGB image to grayscale...")
-                gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
-                print("Converted RGB image to grayscale...")
-                print("Resizing image to 28x28 scale...")
-                img_ = cv2.resize(gray,(280,280))
-                print("Resized...")
-                img_resized = cv2.imwrite(filename='Person1.jpg', img=img_)
-                print("Image saved!")
-
+    #             print("Processing image...")
+    #             img_ = cv2.imread('saved_img.jpg', cv2.IMREAD_ANYCOLOR)
+    #             print("Converting RGB image to grayscale...")
+    #             gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
+    #             print("Converted RGB image to grayscale...")
+    #             print("Resizing image to 28x28 scale...")
+    #             img_ = cv2.resize(gray,(280,280))
+    #             print("Resized...")
+    #             img_resized = cv2.imwrite(filename='./'+ Name +'Person1.jpg', img=img_)
+    #             print("Image saved!")
+            
                 break
             elif key == ord('q'):
                 print("Turning off camera.")
@@ -147,7 +133,7 @@ def caputre():
                 print("Program ended.")
                 cv2.destroyAllWindows()
                 break
-
+            
         except(KeyboardInterrupt):
             print("Turning off camera.")
             webcam.release()
@@ -155,6 +141,9 @@ def caputre():
             print("Program ended.")
             cv2.destroyAllWindows()
             break
-
+        
+        
+capture("Mani")
+    
 
 eel.start('index.html',size = (1024,500),cmdline_args=['--start-fullscreen'])
--- web/main.js
+++ web/main.js
@@ -1,11 +1,53 @@
-var name = document.getElementById("name")
-var number = document.getElementById("number")
-var location = document.getElementById("location")
-var place = document.getElementById("place")
-var itemType = document.getElementById("item-type")
-var item = document.getElementById("item")
-var amount = document.getElementById("amount")
+async function reliver(){
+    let name = document.getElementById("name").value;
+    let number = document.getElementById("number").value;
+    // let output = document.getElementById("output");
+    eel.connectDB()();
+    const result = await eel.fetchUser(name, number)();
+    console.log(name, number)
+    console.log(result["Amount"])
 
-function newUser(){
-    let a = await eel.fetchUser(name,number)();
+    var Elements = ["Name", "Number", "Amount", "S/O W/O D/O", "Location", "Place", "Item", "Pledged Date", "Weight"]; 
+
+    function AddNewElementsByJquery() { 
+        for (var i = 0; i < Elements.length; i++) { 
+        $("#output") 
+        .append('<li>' + Elements[i] + " : " + result[Elements[i]] + '</li>'); 
+        } 
+    } 
+    AddNewElementsByJquery()
+}   
+
+
+async function surrender(){
+    let name = document.getElementById("name").value;
+    let number = document.getElementById("number").value;
+    let location = document.getElementById("location").value;
+    let place = document.getElementById("place").value;
+    let mobile = document.getElementById("mobile").value;
+    let itemType = document.getElementById("item-type").value;
+    let item = document.getElementById("item").value;
+    let amount = document.getElementById("amount").value;
+    let relation = document.getElementById("S/O W/O D/O").value;
+    let pledgedDate = document.getElementById("Pledged Date").value;
+    let weight = document.getElementById("weight").value;
+    document.getElementById("name").value = "";
+    document.getElementById("number").value = "";
+    document.getElementById("location").value = "";
+    document.getElementById("place").value = "";
+    document.getElementById("mobile").value = "";
+    document.getElementById("item-type").value = "";
+    document.getElementById("item").value = "";
+    document.getElementById("amount").value = "";
+    document.getElementById("S/O W/O D/O").value = "";
+    document.getElementById("Pledged Date").value = "";
+    document.getElementById("weight").value = "";
+    await eel.connectDB()();
+    eel.newUser(number, amount, name, relation, location, place, item, itemType, mobile, pledgedDate, weight)();
+}
+
+async function jewel(){
+    // let name = document.getElementById("name").value;
+    await eel.capture("Mani")();
+
 }
